36bb0fd68bde69585666e8eee3981633b995f9cc
William Stam <williamstam@gmail.com>
2024-02-14 20:18:05 +0100

    [script.js] Add fetch and event handler

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,34 @@
+const main = document.getElementById('main')
 
+const hamburgerNav = document.getElementById('hamburgerNav')
+
+const hamburger = document.getElementById('hamburger')
+
+hamburger.addEventListener('click', () => {
+  if (hamburgerNav.classList == 'hamburgerNav') {
+    hamburgerNav.classList.remove('hamburgerNav')
+    hamburgerNav.classList.add('hamburgerOpen')
+  } else {
+    hamburgerNav.classList.remove('hamburgerOpen')
+    hamburgerNav.classList.add('hamburgerNav')
+  }
+})
+
+const openHamburgerMenu = () => {
+  fetch('../../data.json')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (json) {
+      for (let i = 0; i < json.length; i++) {
+        let planet = document.createTextNode(`${json[i].name}`)
+        createHamburgerLink(planet)
+      }
+    })
+}
+
+const createHamburgerLink = (planet) => {
+  hamburgerNav.appendChild(planet)
+}
+
+openHamburgerMenu()
